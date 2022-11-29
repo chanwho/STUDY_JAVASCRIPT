ec4fc83be4fd0bb012fa0a7ba91bc9bce775cfd1
chanwho <anxi0015@gmail.com>
2022-11-30 00:59:26 +0900

    chan

--- docs/browser_js/js/todo_list.js
+++ docs/browser_js/js/todo_list.js
@@ -19,26 +19,32 @@ function addTaskFn(event) {
   if (taskName.value == "") {
     alert("please enter a task");
   }
-  let newTaskText = `<div id="newtask"><div>
+  let newTaskText = `<div id="newtask"><div id = 'newTaskTextbox'>
     <span>${taskName.value}</span>
     </div>
     <div id = 'icon'>
     <div><span><i class="material-icons">delete</i></span></div>
-    <div><span class="material-symbols-outlined">
-    favorite
-    </span></div>
+    <div><span class="material-symbols-outlined">favorite</span></div>
     </div>
     </div>
     `;
   newTasks.insertAdjacentHTML("beforeend", newTaskText);
 }
 
+let heartflag = true;
 queryNewTasks.addEventListener("click", (event) => {
   if (event.target.innerHTML == "delete") {
     event.target.parentElement.parentElement.parentElement.parentElement.remove();
   } else if (event.target.innerHTML == "favorite") {
     let queryheartIcon = document.querySelector(".material-symbols-outlined");
-    queryheartIcon.style =
-      "font-variation-settings:'FILL' 1,'wght' 400, 'GRAD' 0,'opsz' 48";
+    if (heartflag) {
+      queryheartIcon.style =
+        "font-variation-settings:'FILL' 1,'wght' 400, 'GRAD' 0,'opsz' 48";
+      heartflag = false;
+    } else if (heartflag == false) {
+      queryheartIcon.style =
+        "font-variation-settings:'FILL' 0,'wght' 400, 'GRAD' 0,'opsz' 48";
+      heartflag = true;
+    }
   }
 });
